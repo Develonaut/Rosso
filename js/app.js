17f328426533bd244f1e57b31dd9c858341102c2
Ryan McHenry <ryanmmchenry@gmail.com>
2015-03-01 20:07:52 -0500

    Added formating and styling

--- js/app.js
+++ js/app.js
@@ -1,11 +1,42 @@
-$(document).ready(function() {
+var $time = new Date();
+		var $month = $time.getMonth();
+		var $date = $time.getDate();
+		var $day = $time.getDay();
+		var $hours = $time.getHours();
+		var $minutes = $time.getMinutes();
+		var $suffix = "am";
 
-$('.hotspot').hover( function() { 
-		$(this).next().children("img").removeClass('grayscale'); 
-	},
-       function(){ 
-       	$(this).next().children("img").addClass('grayscale'); 
-       }
-);
+		if($minutes < 10) {
+			$minutes = "0" + $minutes;
+		}
 
-});
+		if ($hours >= 12) {
+			$suffix = "PM";
+			$hours = $hours - 12;
+		}
+
+		if ($hours === 0) {
+			$hours = 12;
+		}
+
+		var $days = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+
+		var $months = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+
+		$('.time').html($hours + ":" + $minutes + " " + '<span id="suffix">' + $suffix + '</span>');
+		$('.month').html($months[$month]);
+		$('.date').html($date);
+		$('.day').html($days[$day]);
+		$(function() {
+			$( "#stamp" ).draggable();
+			$('#sBtn').click(function() {
+				$('#stamp').fadeToggle('5000', function() {
+					if ($('#stamp').is(':visible')) {
+						$('#sBtn').val('Hide or Drag'); 
+					} else {
+						$('#sBtn').val('Bring it Back');
+					}
+				});
+			});
+		});
